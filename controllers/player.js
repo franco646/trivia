c169356daa0c6c051202f1665f78b09b8ad4a93b
franco646 <franco@>
2021-03-12 20:19:30 -0300

    Agrega controladores de jugadores

---
+++ controllers/player.js
@@ -0,0 +1,43 @@
+const Player = require('../models/player');
+
+exports.getPlayers = async (req, res, next) => {
+  const { category } = req.params;
+  try {
+    const players = await Player.find({ category });
+    if (!players) {
+      const error = new Error('No players found');
+      error.statusCode = 404;
+      throw error;
+    }
+    return res.status(200).json({ players }).end();
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+    return error;
+  }
+};
+
+exports.postPlayer = async (req, res, next) => {
+  const {
+    name,
+    correctAnswers,
+    category,
+  } = req.body;
+
+  try {
+    await Player.create({
+      name,
+      correctAnswers,
+      category,
+    });
+    return res.status(200).end();
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+    return error;
+  }
+};
