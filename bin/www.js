e1203645d75799c1d840816eda100f3de11f3d17
franco646 <franco@>
2021-03-12 20:18:18 -0300

    Agrega conexión al servidor y a la base de datos

---
+++ bin/www.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-console */
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const app = require('../app');
+
+const mongoServer = new MongoMemoryServer();
+
+const port = process.env.PORT || 8080;
+const env = process.env.NODE_ENV || 'development';
+
+const connect = async () => {
+  if (env === 'test') {
+    const mongoUri = await mongoServer.getUri();
+    await mongoose.connect(mongoUri, {
+      autoReconnect: true,
+      reconnectTries: Number.MAX_VALUE,
+      reconnectInterval: 1000,
+    });
+    app.listen(3000);
+  } else {
+    mongoose
+      .connect(process.env.MONGOOSE_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        app.listen(port, () => {
+          console.log(`server running in port ${port}`);
+        });
+      }).catch((err) => console.log(err));
+  }
+};
+
+connect();
